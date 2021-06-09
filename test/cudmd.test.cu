988ed8b9e0c1f189a4717374f0146b98dfac004f
nikitasa1997 <nikitasa1997@gmail.com>
2021-06-09 13:19:23 +0300

    next

---
+++ test/cudmd.test.cu
@@ -0,0 +1,9 @@
+#include <fstream> // ofstream
+#include <iostream> // ios_base
+
+#include <gtest/gtest.h>
+
+using std::ios_base, std::ofstream;
+
+TEST(CudmdTest, TOOOOOOOOOOOOOODOOOOOOOOOOOOOOO) { 
+}

7b99e4096308b2e5c9f9b46bbb6622a0dbc47c18
nikitasa1997 <nikitasa1997@gmail.com>
2021-06-09 22:46:51 +0300

    next

--- include/cudmd/error_handling.h
+++ include/cudmd/error_handling.h
@@ -3,38 +3,58 @@
 
 #include <stdexcept> // runtime_error
 #include <string> // string
-#include <type_traits> // is_same
+#include <type_traits> // is_enum
 
-#include <cublas_v2.h> //
-#include <cusolverDn.h> //
-
-template<typename Status>
+template<typename Code>
 class basic_error final : public std::runtime_error {
 public:
-    basic_error(Status, const std::string &);
-    basic_error(Status, const char *);
-    basic_error(Status);
-    basic_error(const basic_error &) noexcept = default;
-    basic_error &operator=(const basic_error &) noexcept = default;
-    virtual ~basic_error() noexcept;
+    __host__ inline basic_error(Code, const std::string &);
+    __host__ inline basic_error(Code, const char *);
+    __host__ inline basic_error(Code = Code());
+    __host__ inline basic_error(const basic_error &) noexcept = default;
+    __host__ inline basic_error &operator=(const basic_error &) noexcept = default;
+    __host__ inline ~basic_error() noexcept override = default;
 
-    Status code() const noexcept;
-    const char *what() const noexcept override;
+    __host__ inline Code code() const noexcept;
+    __host__ inline const char *what() const noexcept override;
 
 private:
-    static_assert(
-        std::is_same<Status, cublasStatus_t>::value ||
-        std::is_same<Status, cusolverStatus_t>::value,
-        "template argument Status must have type either"
-        "cublasStatus_t or cusolverStatus_t"
+    static_assert(std::is_enum<Code>::value,
+        "template argument Code must be enum"
     );
 
-    Status status_;
+    Code code_;
 };
 
-template<class Status>
-inline void throw_if_error(const Status status) {
-    
+template<typename Code>
+__host__ inline basic_error<Code>::basic_error(
+    const Code code,
+    const std::string &what
+) : runtime_error(what), code_(code) {}
+
+template<typename Code>
+__host__ inline basic_error<Code>::basic_error(
+    const Code code,
+    const char * const what
+) : runtime_error(what), code_(code) {}
+
+template<typename Code>
+__host__ inline basic_error<Code>::basic_error(
+    const Code code
+) : runtime_error(""), code_(code) {}
+
+template<typename Code>
+__host__ inline Code basic_error<Code>::code() const noexcept {
+    return code_;
+}
+
+template<typename Code>
+__host__ inline const char *basic_error<Code>::what() const noexcept {
+    return std::runtime_error::what();
+}
+
+template<class Code>
+__host__ inline void throw_if_error(const Code code) {
 }
 
 #endif
--- src/main.cu
+++ src/main.cu
@@ -1,12 +1,32 @@
 #include <cassert>
 #include <cstddef>
 
+#include <iostream>
+
 #include <cublas_v2.h>
 #include <cuda_runtime.h>
-#include <cusolverDn.h>
 
 #include <cudmd/error_handling.h>
 
+using cublas_error = basic_error<cublasStatus_t>;
+
+__device__ inline int sum3(const int a, const int b, const int c) {
+    return a + b + c;
+}
+
+__global__ __forceinline__ void hcd() {
+    const int a = 5, b = 7, c = 11;
+    const int d = sum3(a, b, c);
+}
+
 int main() {
+    cublas_error
+    	g(CUBLAS_STATUS_SUCCESS, "4545"),
+    	h(CUBLAS_STATUS_SUCCESS, std::string("test")),
+    	i(CUBLAS_STATUS_SUCCESS),
+    	j(CUBLAS_STATUS_SUCCESS);
+    std::cout << g.what() << '\n' << i.code();
+    hcd<<<1, 1>>>();
+
     return 0;
 }

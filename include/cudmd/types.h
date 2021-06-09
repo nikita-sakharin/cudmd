9fc814b08ab5e2f0756167baf4fdd7ea0d63c88c
Nikita <nikitasa1997@gmail.com>
2021-06-09 10:11:42 +0300

    next

---
+++ include/cudmd/types.h
@@ -0,0 +1,16 @@
+#ifndef CUDMD_TYPES_H
+#define CUDMD_TYPES_H
+
+typedef signed char schar;
+typedef unsigned char uchar;
+typedef short shrt;
+typedef unsigned short ushrt;
+typedef unsigned uint;
+typedef unsigned long ulong;
+typedef long long llong;
+typedef unsigned long long ullong;
+typedef float flt;
+typedef double dbl;
+typedef long double ldbl;
+
+#endif

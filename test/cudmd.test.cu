71bc1ee1969a83be0495c2920514afbd1e4c3166
nikitasa1997 <nikitasa1997@gmail.com>
2021-06-11 01:44:50 +0300

    next

--- test/cudmd.test.cu
+++ test/cudmd.test.cu
@@ -1,13 +1,41 @@
-#include <fstream> // ofstream
-#include <iostream> // ios_base
+#include <cstddef> // size_t
+
+#include <math_constants.h> // CUDART_PI
+
+#include <thrust/complex.h> // complex
+#include <thrust/device_vector.h> // device_vector
+#include <thrust/generate.h> // generate
+#include <thrust/host_vector.h> // host_vector
 
 #include <gtest/gtest.h>
 
-#include <cudmd/types.h>
+#include <cudmd/types.h> // dbl
 
-using std::ios_base;
-using std::ofstream;
+using std::size_t;
+using thrust::complex;
+using thrust::device_vector;
+using thrust::generate;
+using thrust::host_vector;
 
-TEST(CudmdTest, ToDo) {
+TEST(CudmdTest, First) {
+    static const size_t x_size = (1U << 10) + 1U, t_size = (1U << 8) + 1U;
+    static const complex<dbl> x_start = -5.0, x_stop = 5.0;
+    static const dbl t_start = 0.0, t_stop = 4.0 * CUDART_PI;
+
+    host_vector<complex<dbl>> host_x_upper(x_size * t_size);
+    generate(x_upper.begin(), x_upper.end(),
+        [n = 0U]() mutable noexcept -> complex<dbl> {
+            const size_t i = n / x_size, j = n % x_size;
+            const complex<dbl>
+                x = x_start + j * (x_stop - x_start) / (x_size - 1U);
+            const dbl t = t_start + i * (t_stop - t_start) / (t_size - 1U);
+            const complex<dbl>
+                f1 = 1.0 / cosh(x + 3.0) * exp(complex<dbl>(0.0, 2.3 * t)),
+                f2 = 2.0 / cosh(x) * tanh(x) * exp(complex<dbl>(0.0, 2.8 * t));
+            return f1 + f2;
+        }
+    );
+    device_vector<complex<dbl>> device_x_upper = host_x_upper;
+    // cudmd(x, );
     ASSERT_EQ(1, 1);
 }

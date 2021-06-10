a50b6ba9661974daa355752d0b8539c890de65ce
nikitasa1997 <nikitasa1997@gmail.com>
2021-06-10 11:13:15 +0300

    next

--- include/cudmd/types.h
+++ include/cudmd/types.h
@@ -1,16 +1,16 @@
 #ifndef CUDMD_TYPES_H
 #define CUDMD_TYPES_H
 
-typedef signed char schar;
-typedef unsigned char uchar;
-typedef short shrt;
-typedef unsigned short ushrt;
-typedef unsigned uint;
-typedef unsigned long ulong;
-typedef long long llong;
-typedef unsigned long long ullong;
-typedef float flt;
-typedef double dbl;
-typedef long double ldbl;
+using schar  = signed char;
+using uchar  = unsigned char;
+using shrt   = short;
+using ushrt  = unsigned short;
+using uint   = unsigned;
+using ulong  = unsigned long;
+using llong  = long long;
+using ullong = unsigned long long;
+using flt    = float;
+using dbl    = double;
+using ldbl   = long double;
 
 #endif

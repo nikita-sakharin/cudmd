d56b4f152f792a3d4aae96d323d148c2b42bf66d
nikitasa1997 <nikitasa1997@gmail.com>
2021-06-10 22:19:29 +0300

    next

--- include/cudmd/cudmd.h
+++ include/cudmd/cudmd.h
@@ -1,6 +1,10 @@
 #ifndef CUDMD_CUDMD_H
 #define CUDMD_CUDMD_H
 
+#include <cstdint> // int64_t
+
+#include <cuComplex.h> // cuDoubleComplex
+
 #include <thrust/device_ptr.h> // device_ptr
 #include <thrust/device_vector.h> // device_vector
 #include <thrust/tuple.h> // tuple
@@ -11,8 +15,8 @@ __host__ thrust::tuple<
     thrust::device_vector<cuDoubleComplex>
 > cudmd(
     thrust::device_ptr<cuDoubleComplex>,
-    int64_t, int64_t, int64_t k,
-    int64_t = 2 * k, int64_t = 2,
+    std::int64_t, std::int64_t, std::int64_t k,
+    std::int64_t = 2 * k, std::int64_t = 2,
 );
 
 #endif

45fc07ed9ec8fcc3631af2a65be7a6268e5436ac
Nikita <nikitasa1997@gmail.com>
2021-06-10 19:04:20 +0300

    next

--- include/cudmd/cublas_helpers.h
+++ include/cudmd/cublas_helpers.h
@@ -1,10 +1,10 @@
 #ifndef CUDMD_CUBLAS_HELPERS_H
 #define CUDMD_CUBLAS_HELPERS_H
 
-#include <cublas_v2.h>
+#include <cublas_v2.h> // cublas*
 
-#include <cudmd/error_handling.h>
-#include <cudmd/handle.h>
+#include <cudmd/error_handling.h> // basic_error
+#include <cudmd/handle.h> // basic_handle
 
 using cublas_error = basic_error<cublasStatus_t>;
 using cublas_handle = basic_handle<cublasHandle_t, cublasStatus_t,
--- include/cudmd/cusolverDn_helpers.h
+++ include/cudmd/cusolverDn_helpers.h
@@ -1,10 +1,10 @@
 #ifndef CUDMD_SOLVER_DN_HELPERS_H
 #define CUDMD_SOLVER_DN_HELPERS_H
 
-#include <cusolverDn.h>
+#include <cusolverDn.h> // cusolverDn*, cusolverStatus_t
 
-#include <cudmd/error_handling.h>
-#include <cudmd/handle.h>
+#include <cudmd/error_handling.h> // basic_error
+#include <cudmd/handle.h> // basic_handle
 
 using cusolver_error = basic_error<cusolverStatus_t>;
 using cusolverDn_handle = basic_handle<cusolverDnHandle_t, cusolverStatus_t,
